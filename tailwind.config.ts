67cf776d429b718de19705efab8ca9f95727df7b
loikloikpoom <loikloikpoom@gmail.com>
2023-08-18 19:45:31 +0700

    update tw config 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  content: ["./src/app/**/*.{ts,tsx}", "./src/Components/**/*.{ts,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
