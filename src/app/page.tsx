fc9145ae791b29201707199c40cd1d7a139d7560
PKMPOOM <loikpoom@gmail.com>
2023-11-26 13:43:07 +0700

    Redirect admin to time table page instead of dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (session && session.user.role === "admin") {
-    redirect("/dashboard");
+    redirect("/time_table");
   } else {
     redirect("/subjects");
   }
